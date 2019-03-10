1809dc30da68cbeacb3466cb961a14667a88b634
mariux255 <44610785+mariux255@users.noreply.github.com>
2019-03-10 23:06:06 +0100

    First commit

--- src/Coordination.java
+++ src/Coordination.java
@@ -1,2 +1,22 @@
-public class Coordination {
+public class Coordination implements CoordinationInterface {
+    private int[][] B;
+    public int[][] initialize(){
+        B = new int[8][8];
+        for(int i=0; i<3;i++){
+            for(int j=0; j<8; j++){
+                if ((i+j)%2==0)
+                    B[i][j]=1;
+            }
+        }
+        for(int i=5; i<8;i++) {
+            for (int j = 0; j < 8; j++) {
+                if ((i + j) % 2 == 0){
+                    B[i][j] = 2;
+            }}}
+    return B;
+    }
+    public int PieceValue(int coordinate1, int coordinate2){
+        return B[coordinate1][coordinate2];
+
+    }
 }
